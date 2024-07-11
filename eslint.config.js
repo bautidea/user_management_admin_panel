8202414685d61acfe0d96acde3354a15b7c9b8c5
bautidea <juanbautistadeangelis@gmail.com>
2024-07-11 09:12:37 -0300

    Starting linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+import { rules } from "eslint-plugin-react/configs/all";
 
-export default {
-  overrides: [
-    {
-      files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-      parser: tsParser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      plugins: ["@typescript-eslint", "react"],
-      rules: {
-        "react/react-in-jsx-scope": "off",
-      },
-    },
-  ],
-  settings: {
-    react: {
-      version: "detect",
-    },
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    rules : {
+      'react/react-in-jsx-scope' : 'off',
+    'react-hooks/exhaustive-deps' : 'off'
+    }
   },
-  globals: globals.browser,
-  ...pluginJs.configs.recommended,
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...pluginReactConfig,
-};
+  ...fixupConfigRules(pluginReactConfig),
+];
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
-  const [listOfUsers, setListOfUsers] = useState();
+  const [listOfUsers, setListOfUsers] = useState([]);
 
   useEffect(() => {
     const apiLink =
       'https://randomuser.me/api/?results=10&inc=picture,name,location';
 
-    fetch(apiLink)
-      .then((response) => response.json())
-      .then((data) => setListOfUsers(data.results))
-      .catch((err) => console.log(`Error when connecting to DB ${err}`));
+      const fetchData = async () => {
+        return fetch(apiLink)
+        .then((response) => response.json())
+        .then((data) => setListOfUsers(data.results))
+        .catch((err) => console.log(`Error when connecting to DB ${err}`));
+      }
+    
+      fetchData()
   }, []);
-
+  
   console.log(listOfUsers);
   return (
     <>

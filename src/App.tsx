8bd0e96ade33592d031e03c7f013846f82314c54
bautidea <juanbautistadeangelis@gmail.com>
2024-07-11 09:17:10 -0300

    fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,11 @@ function App() {
   useEffect(() => {
     const apiLink =
       'https://randomuser.me/api/?results=10&inc=picture,name,location';
-
-      const fetchData = async () => {
-        return fetch(apiLink)
-        .then((response) => response.json())
-        .then((data) => setListOfUsers(data.results))
-        .catch((err) => console.log(`Error when connecting to DB ${err}`));
-      }
     
-      fetchData()
+    fetch(apiLink)
+      .then((response) => response.json())
+      .then((data) => setListOfUsers(data.results))
+      .catch((err) => console.log(`Error when connecting to DB ${err}`));
   }, []);
   
   console.log(listOfUsers);

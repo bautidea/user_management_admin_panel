c5e221e7c498f8bde09efe48d92edfa01e012820
bautidea <juanbautistadeangelis@gmail.com>
2024-07-13 11:43:47 -0300

    Updating fetch logic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import { fetchUsers } from './services/fetchUsers';
+import { useUsers } from './hooks/useUsers';
 
 function App() {
-  const [listOfUsers, setListOfUsers] = useState([]);
+  const { listOfUsers, updateListOfUsers } = useUsers();
 
   useEffect(() => {
     async function fetchData() {
       const data = await fetchUsers();
-      setListOfUsers(data);
+      updateListOfUsers(data);
     }
 
     fetchData();
---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { ListOfUsers } from '../types';
+
+export function useUsers() {
+  const [listOfUsers, setListOfUsers] = useState<ListOfUsers>();
+
+  function updateListOfUsers(users: ListOfUsers) {
+    return setListOfUsers(users);
+  }
+
+  return { listOfUsers, updateListOfUsers };
+}
